27a385263b9ceeeb9d34dacb3e4f32e64cec1cbd
nownabe <nownabe@gmail.com>
2023-07-16 00:21:16 +0900

    Format setup.sh

--- setup.sh
+++ setup.sh
@@ -1,49 +1,49 @@
 #!/usr/bin/env bash
 
 echo_g() {
-  echo -e "\033[1;32m$*\033[m"
+	echo -e "\033[1;32m$*\033[m"
 }
 
 ensure_directories() {
-  [[ -e "$HOME/.cache" ]] || mkdir -p $HOME/.cache
-  [[ -e "$HOME/.cache/shell" ]] || mkdir -p $HOME/.cache/shell
+	[[ -e "$HOME/.cache" ]] || mkdir -p "$HOME/.cache"
+	[[ -e "$HOME/.cache/shell" ]] || mkdir -p "$HOME/.cache/shell"
 }
 
 ensure_zplug() {
-  if [[ ! -d "$HOME/.zplug" ]]; then
-    git clone https://github.com/zplug/zplug "$HOME/.zplug"
-  fi
+	if [[ ! -d "$HOME/.zplug" ]]; then
+		git clone https://github.com/zplug/zplug "$HOME/.zplug"
+	fi
 }
 
 ensure_symlinks() {
-  local basedir name src dst dstdir
+	local basedir name src dst dstdir
 
-  basedir="$(cd "$(dirname "$0")" && pwd)"
+	basedir="$(cd "$(dirname "$0")" && pwd)"
 
-  find "$basedir" -type f | grep -v /.git/ | while read -r src; do
-    name=$(basename "$src")
+	find "$basedir" -type f | grep -v /.git/ | while read -r src; do
+		name=$(basename "$src")
 
-    [[ "$name" = ".gitignore" ]] && continue
-    [[ "$name" = "setup.sh" ]] && continue
-    [[ "$name" = "README.md" ]] && continue
+		[[ "$name" = ".gitignore" ]] && continue
+		[[ "$name" = "setup.sh" ]] && continue
+		[[ "$name" = "README.md" ]] && continue
 
-    dst=$HOME/$name
-    echo -n "$dst : "
+		dst=$HOME/$name
+		echo -n "$dst : "
 
-    if [[ -L $dst ]]; then
-      echo "already exists 😘"
-    else
-      [[ -e $dst ]] && mv "$dst" "$dst.badkup"
+		if [[ -L $dst ]]; then
+			echo "already exists 😘"
+		else
+			[[ -e $dst ]] && mv "$dst" "$dst.badkup"
 
-      dstdir="$(dirname "$dst")"
-      if [[ ! -e "$dstdir" ]]; then
-        mkdir -p "$dstdir"
-      fi
+			dstdir="$(dirname "$dst")"
+			if [[ ! -e "$dstdir" ]]; then
+				mkdir -p "$dstdir"
+			fi
 
-      ln -s "$src" "$dst"
-      echo_g "created new symbolic link! 🥳"
-    fi
-  done
+			ln -s "$src" "$dst"
+			echo_g "created new symbolic link! 🥳"
+		fi
+	done
 }
 
 ensure_directories
