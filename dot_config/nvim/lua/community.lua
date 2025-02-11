d48a0beb3ae2d39e2be71cdaa9761a2cb3dc4b1a
nownabe <1286807+nownabe@users.noreply.github.com>
2025-02-11 14:36:04 +0900

    feat(nvim): Enable astrocommunity

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -1,8 +1,8 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
+-- 
+-- https://github.com/AstroNvim/astrocommunity
 
 ---@type LazySpec
 return {
