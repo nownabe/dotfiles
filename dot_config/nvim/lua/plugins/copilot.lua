b0cde3de9df1bf511c37df0fb4da3ea38cdf9767
nownabe <1286807+nownabe@users.noreply.github.com>
2025-02-11 20:31:33 +0900

    chore(nvim): Disable copilot.lua panel

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -16,22 +16,7 @@ return {
             dismiss = "<C-]>",
           },
         },
-        panel = {
-          -- enabled = true,
-          enabled = false,
-          auto_refresh = false,
-          keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept    = "<CR>",
-            refresh   = "gr",
-            open      = "<M-CR>",
-          },
-          layout = {
-            position = "bottom",
-            ratio = 0.4,
-          },
-        },
+        panel = { enabled = false },
         copilot_node_command = "node",
         server_opts_overrides = {},
       })
