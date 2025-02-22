78cb591ad145e5c799aab23804db421cfeb05ba4
nownabe <1286807+nownabe@users.noreply.github.com>
2025-02-22 16:16:41 +0900

    chore(nvim): Update key mappings

--- dot_config/nvim/lua/plugins/mappings.lua
+++ dot_config/nvim/lua/plugins/mappings.lua
@@ -6,17 +6,27 @@ local get_icon = require("astroui").get_icon
 return {
   {
     "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
-    opts = {
-      mappings = {
-        n = {
-          -- Cursor
-          ["<leader>h"] = { "^", desc = "Move to first non-whitespace" },
-          ["<leader>l"] = { "$", desc = "Move to the end of the line" },
-          ["<leader>m"] = { "%", desc = "Match nearest [], (), {}" },
-        },
-      },
-    },
+    ---@params opts AstroCoreOpts
+    opts = function(_, opts)
+      local maps = astrocore.empty_map_table()
+
+      -- Sections
+      maps.n["<Leader>a"] = { desc = "Avante" }
+      maps.n["<Leader>c"] = { desc = "Copilot" }
+
+      -- Cursor
+      maps.n["<Leader>h"] = { "^", desc = "Move to first non-whitespace" }
+      maps.n["<Leader>l"] = { "$", desc = "Move to the end of the line" }
+      maps.n["<Leader>m"] = { "%", desc = "Match nearest [], (), {}" }
+
+      -- LSP
+      maps.n["<Leader>ld"] = false
+      maps.n["<Leader>lD"] = false
+      maps.n["<Leader>ls"] = false
+      maps.n["<Leader>lS"] = false
+
+      opts.mappings = astrocore.extend_tbl(opts.mappings, maps)
+    end,
   },
   {
     -- ref. https://github.com/AstroNvim/AstroNvim/blob/87dc70f7761c3e9965315182f0c40bfbd2cbcd0e/lua/astronvim/plugins/_astrolsp_mappings.lua
@@ -25,30 +35,30 @@ return {
     opts = function(_, opts)
       local maps = astrocore.empty_map_table()
 
-      maps.n["<leader>;"] = { desc = get_icon("ActiveLSP", 1, true) .. "Language Tools" }
-      maps.v["<leader>;"] = { desc = get_icon("ActiveLSP", 1, true) .. "Language Tools" }
+      maps.n["<Leader>;"] = { desc = get_icon("ActiveLSP", 1, true) .. "Language Tools" }
+      maps.v["<Leader>;"] = { desc = get_icon("ActiveLSP", 1, true) .. "Language Tools" }
 
-      maps.n["<leader>;a"] = {
+      maps.n["<Leader>;a"] = {
         function() vim.lsp.buf.code_action() end,
         desc = "LSP code action",
         cond = "textDocument/codeAction",
       }
-      maps.x["<leader>;a"] = {
+      maps.x["<Leader>;a"] = {
         function() vim.lsp.buf.code_action() end,
         desc = "LSP code action",
         cond = "textDocument/codeAction",
       }
-      maps.n["<leader>;A"] = {
+      maps.n["<Leader>;A"] = {
         function() vim.lsp.buf.code_action { context = { only = { "source" }, diagnostics = {} } } end,
         desc = "LSP source action",
         cond = "textDocument/codeAction",
       }
 
-      maps.n["<leader>;l"] =
+      maps.n["<Leader>;l"] =
         { function() vim.lsp.codelens.refresh() end, desc = "LSP CodeLens refresh", cond = "textDocument/codeLens" }
-      maps.n["<leader>;L"] =
+      maps.n["<Leader>;L"] =
         { function() vim.lsp.codelens.run() end, desc = "LSP CodeLens run", cond = "textDocument/codeLens" }
-      maps.n["<leader>uL"] = {
+      maps.n["<Leader>uL"] = {
         function() require("astrolsp.toggles").codelens() end,
         desc = "Toggle CodeLens",
         cond = "textDocument/codeLens",
@@ -56,17 +66,17 @@ return {
 
       maps.n["gD"] = false
       maps.n["gd"] = false
-      maps.n["<leader>;D"] = {
+      maps.n["<Leader>;D"] = {
         function() vim.lsp.buf.declaration() end,
         desc = "Declaration of current symbol",
         cond = "textDocument/declaration",
       }
-      maps.n["<leader>;d"] = {
+      maps.n["<Leader>;d"] = {
         function() vim.lsp.buf.definition() end,
         desc = "Show the definition of current symbol",
         cond = "textDocument/definition",
       }
-      -- maps.n["<leader>;d"] = {
+      -- maps.n["<Leader>;d"] = {
       --   function() require("telescope.builtin").lsp_definitions() end,
       --   desc = "Definition of current symbol",
       -- },
@@ -79,98 +89,103 @@ return {
         end
       end
       local formatting_enabled = formatting_checker()
-      maps.n["<leader>;f"] = {
+      maps.n["<Leader>;f"] = {
         function() vim.lsp.buf.format(require("astrolsp").format_opts) end,
         desc = "Format buffer",
         cond = formatting_enabled,
       }
-      maps.v["<leader>;f"] = {
+      maps.v["<Leader>;f"] = {
         function() vim.lsp.buf.format(require("astrolsp").format_opts) end,
         desc = "Format buffer",
         cond = formatting_checker "rangeFormatting",
       }
-      maps.n["<leader>uf"] = {
+      maps.n["<Leader>uf"] = {
         function() require("astrolsp.toggles").buffer_autoformat() end,
         desc = "Toggle autoformatting (buffer)",
         cond = formatting_enabled,
       }
-      maps.n["<leader>uF"] = {
+      maps.n["<Leader>uF"] = {
         function() require("astrolsp.toggles").autoformat() end,
         desc = "Toggle autoformatting (global)",
         cond = formatting_enabled,
       }
 
-      maps.n["<leader>u?"] = {
+      maps.n["<Leader>u?"] = {
         function() require("astrolsp.toggles").signature_help() end,
         desc = "Toggle automatic signature help",
         cond = "textDocument/signatureHelp",
       }
 
       maps.n["gI"] = false
-      -- maps.n["<leader>;i"] = {
+      -- maps.n["<Leader>;i"] = {
       --   function() vim.lsp.buf.implementation() end,
       --   desc = "Implementation of current symbol",
       --   cond = "textDocument/implementation",
       -- }
-      maps.n["<leader>;i"] = {
+      maps.n["<Leader>;i"] = {
         function() require("telescope.builtin").lsp_implementations() end,
         desc = "Implementation of current symbol",
       }
 
-      maps.n["<leader>uh"] = {
+      maps.n["<Leader>uh"] = {
         function() require("astrolsp.toggles").buffer_inlay_hints() end,
         desc = "Toggle LSP inlay hints (buffer)",
         cond = vim.lsp.inlay_hint and "textDocument/inlayHint" or false,
       }
-      maps.n["<leader>uH"] = {
+      maps.n["<Leader>uH"] = {
         function() require("astrolsp.toggles").inlay_hints() end,
         desc = "Toggle LSP inlay hints (global)",
         cond = vim.lsp.inlay_hint and "textDocument/inlayHint" or false,
       }
 
-      -- maps.n["<leader>;R"] =
+      -- maps.n["<Leader>;R"] =
       --   { function() vim.lsp.buf.references() end, desc = "Search references", cond = "textDocument/references" }
-      maps.n["<leader>;R"] = {
+      maps.n["<Leader>;R"] = {
         function() require("telescope.builtin").lsp_references() end,
         desc = "Search references",
       }
 
-      maps.n["<leader>;r"] = {
+      maps.n["<Leader>;r"] = {
         function() vim.lsp.buf.rename() end,
         desc = "Rename current symbol",
         cond = "textDocument/rename",
       }
 
-      maps.n["<leader>;s"] = {
+      maps.n["<Leader>;s"] = {
         function() vim.lsp.buf.signature_help() end,
         desc = "Signature help",
         cond = "textDocument/signatureHelp",
       }
 
-      maps.n["<leader>;h"] = {
+      maps.n["<Leader>;h"] = {
         function() vim.lsp.buf.hover() end,
         desc = "Hover symbol details",
         cond = "textDocument/hover",
       }
 
-      maps.n["<leader>;t"] = {
+      maps.n["<Leader>;t"] = {
         function() vim.lsp.buf.type_definition() end,
         desc = "Definition of current type",
         cond = "textDocument/typeDefinition",
       }
       maps.n["gy"] = false
 
-      -- maps.n["<leader>;S"] = {
+      -- maps.n["<Leader>;S"] = {
       --   function() vim.lsp.buf.workspace_symbol() end,
       --   desc = "Search workspace symbols",
       --   cond = "workspace/symbol",
       -- }
-      maps.n["<leader>;S"] = {
+      maps.n["<Leader>;S"] = {
         function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end,
         desc = "Search workspace symbols",
       }
 
-      maps.n["<leader>uY"] = {
+      maps.n["<Leader>;o"] = {
+        function() require("aerial").toggle() end,
+        desc = "Symbols outline",
+      }
+
+      maps.n["<Leader>uY"] = {
         function() require("astrolsp.toggles").buffer_semantic_tokens() end,
         desc = "Toggle LSP semantic highlight (buffer)",
         cond = function(client)
@@ -178,16 +193,16 @@ return {
         end,
       }
 
-      maps.n["<leader>;n"] = {
+      maps.n["<Leader>;n"] = {
         function() vim.diagnostic.open_float() end,
         desc = "Hover diagnostics",
       }
-      maps.n["<leader>;N"] = {
+      maps.n["<Leader>;N"] = {
         function() require("telescope.builtin").diagnostics() end,
         desc = "Search diagnostics",
       }
 
-      opts.mappings = astrocore.extend_tbl(opts.mappings, maps)
+      opts.mappings = maps
     end,
   },
 }
