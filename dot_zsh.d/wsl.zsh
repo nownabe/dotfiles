5dfc5daf1b6ce976a3174a7b838a3e0a74640bc9
nownabe <1286807+nownabe@users.noreply.github.com>
2025-06-15 20:46:15 +0900

    chore(zsh): update WSL config

--- dot_zsh.d/wsl.zsh
+++ dot_zsh.d/wsl.zsh
@@ -1,1 +1,7 @@
-export GPG_TTY="$(tty)"
+if grep -qi "WSL2" /proc/version; then
+  echo -e "\033[1;32m🐧 Running inside WSL2\033[0m"
+  export IS_WSL="true"
+  
+  export GPG_TTY="$(tty)"
+  export BROWSER="/mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe"
+fi
