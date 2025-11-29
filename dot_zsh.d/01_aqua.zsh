d55a608d7e1f881b24ebbdb456cda2826ef1cb06
nownabe <1286807+nownabe@users.noreply.github.com>
2025-11-29 14:14:55 +0900

    feat(zsh): enable aqua

---
+++ dot_zsh.d/01_aqua.zsh
@@ -0,0 +1,2 @@
+export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
+export AQUA_GLOBAL_CONFIG="${AQUA_GLOBAL_CONFIG:-}:${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua/aqua.yaml"
