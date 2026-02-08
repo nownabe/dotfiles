2f58c98e4fa7a3843f7fc59221d63162601a7a18
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 21:55:29 +0900

    fix(nvim): set indent line blend to 100 (test)

--- programs/nvim/config/lua/plugins/snacks.lua
+++ programs/nvim/config/lua/plugins/snacks.lua
@@ -1,9 +1,9 @@
 return {
   "folke/snacks.nvim",
   init = function()
-    vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#0a0a14", blend = 98 })
+    vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#0a0a14", blend = 100 })
     vim.api.nvim_create_autocmd("ColorScheme", {
-      callback = function() vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#0a0a14", blend = 98 }) end,
+      callback = function() vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#0a0a14", blend = 100 }) end,
     })
   end,
   opts = {
