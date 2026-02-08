40003116194a809ef9de285eebcd12a9e64a6c36
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 15:55:36 +0900

    refactor(nvim): define keymaps entirely via which-key spec

--- programs/nvim/config/lua/plugins/keymaps.lua
+++ programs/nvim/config/lua/plugins/keymaps.lua
@@ -1,24 +1,11 @@
 return {
   {
-    "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
-    opts = {
-      mappings = {
-        n = {
-          ["<Leader>h"] = { "^", desc = "Move to first non-whitespace" },
-          ["<Leader>l"] = { "$", desc = "Move to end of line" },
-          ["<Leader>m"] = { "%", desc = "Match nearest [], (), {}" },
-        },
-      },
-    },
-  },
-  {
     "folke/which-key.nvim",
     opts = {
       spec = {
-        { "<Leader>h", icon = "󰜲" },
-        { "<Leader>l", icon = "󰜵" },
-        { "<Leader>m", icon = "󰅪" },
+        { "<Leader>h", "^", desc = "Move to first non-whitespace", icon = "󰜲" },
+        { "<Leader>l", "$", desc = "Move to end of line", icon = "󰜵" },
+        { "<Leader>m", "%", desc = "Match nearest [], (), {}", icon = "󰅪" },
       },
     },
   },
