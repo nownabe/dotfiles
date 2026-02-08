6162e255cdea31538f407fb1c487c0720f5b237e
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 22:49:33 +0900

    feat(nvim): migrate basic options and LSP settings

---
+++ programs/nvim/config/lua/plugins/astrolsp.lua
@@ -0,0 +1,29 @@
+return {
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    formatting = {
+      format_on_save = {
+        enabled = true,
+      },
+      timeout_ms = 1000,
+    },
+    features = {
+      codelens = true,
+      inlay_hints = false,
+      semantic_tokens = true,
+    },
+    autocmds = {
+      lsp_codelens_refresh = {
+        cond = "textDocument/codeLens",
+        {
+          event = { "InsertLeave", "BufEnter" },
+          desc = "Refresh CodeLens",
+          callback = function(args)
+            if require("astrolsp").config.features.codelens then vim.lsp.codelens.refresh { bufnr = args.buf } end
+          end,
+        },
+      },
+    },
+  },
+}
