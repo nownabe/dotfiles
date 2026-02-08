37816c5c249383c60c84c3c521c9dc8adf9bc6d8
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 21:48:46 +0900

    fix(nvim): make indent guide lines barely visible

--- programs/nvim/config/lua/plugins/snacks.lua
+++ programs/nvim/config/lua/plugins/snacks.lua
@@ -1,9 +1,9 @@
 return {
   "folke/snacks.nvim",
   init = function()
-    vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#ff0000" })
+    vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#0a0a14" })
     vim.api.nvim_create_autocmd("ColorScheme", {
-      callback = function() vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#ff0000" }) end,
+      callback = function() vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#0a0a14" }) end,
     })
   end,
   opts = {
