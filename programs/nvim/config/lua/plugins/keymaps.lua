5e268d481341e60327ae50828f32e662ae2c7a0f
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 16:09:15 +0900

    refactor(nvim): use opts function to merge which-key spec

--- programs/nvim/config/lua/plugins/keymaps.lua
+++ programs/nvim/config/lua/plugins/keymaps.lua
@@ -1,13 +1,15 @@
 return {
   {
     "folke/which-key.nvim",
-    opts_extend = { "spec" },
-    opts = {
-      spec = {
+    opts = function(_, opts)
+      local spec = {
         { "<Leader>h", "^", desc = "Move to first non-whitespace", icon = "󰜲", order = 1 },
         { "<Leader>l", "$", desc = "Move to end of line", icon = "󰜵", order = 2 },
         { "<Leader>m", "%", desc = "Match nearest [], (), {}", icon = "󰅪", order = 3 },
-      },
-    },
+      }
+      for _, s in ipairs(spec) do
+        table.insert(opts.spec, s)
+      end
+    end,
   },
 }
