3376b19745e394880e94e3fa0e619d58fbdfdd35
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 22:22:42 +0900

    fix(nvim): remove SnacksIndent color override (test default)

--- programs/nvim/config/lua/plugins/colorscheme.lua
+++ programs/nvim/config/lua/plugins/colorscheme.lua
@@ -15,7 +15,6 @@ return {
           NormalFloat = { bg = "NONE" },
           FloatBorder = { bg = "NONE" },
           FloatTitle = { bg = "NONE" },
-          SnacksIndent = { fg = "#181825" },
         }
       end,
     },
