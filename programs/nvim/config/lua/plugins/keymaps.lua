fd7f042ab77a011506a4b7fdaab2f262abfd4048
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 16:10:11 +0900

    refactor(nvim): use vim.list_extend for which-key spec merge

--- programs/nvim/config/lua/plugins/keymaps.lua
+++ programs/nvim/config/lua/plugins/keymaps.lua
@@ -2,14 +2,11 @@ return {
   {
     "folke/which-key.nvim",
     opts = function(_, opts)
-      local spec = {
+      vim.list_extend(opts.spec, {
         { "<Leader>h", "^", desc = "Move to first non-whitespace", icon = "󰜲", order = 1 },
         { "<Leader>l", "$", desc = "Move to end of line", icon = "󰜵", order = 2 },
         { "<Leader>m", "%", desc = "Match nearest [], (), {}", icon = "󰅪", order = 3 },
-      }
-      for _, s in ipairs(spec) do
-        table.insert(opts.spec, s)
-      end
+      })
     end,
   },
 }
