63fdf51ed1d7783420bb73ef94869944b1a5b6ff
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 21:56:17 +0900

    fix(nvim): disable indent guides, keep scope only

--- programs/nvim/config/lua/plugins/snacks.lua
+++ programs/nvim/config/lua/plugins/snacks.lua
@@ -1,12 +1,11 @@
 return {
   "folke/snacks.nvim",
-  init = function()
-    vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#0a0a14", blend = 100 })
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      callback = function() vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#0a0a14", blend = 100 }) end,
-    })
-  end,
   opts = {
+    indent = {
+      indent = {
+        enabled = false,
+      },
+    },
     picker = {
       win = {
         input = {
