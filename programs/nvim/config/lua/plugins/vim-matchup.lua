3234f566452d855acefd2d4097261864fc889b31
Shogo Watanabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 23:48:09 +0900

    feat(nvim): add vim-matchup for enhanced bracket matching (#142)
    ## Summary
    - Add vim-matchup plugin for enhanced `%` motion
    - Extends bracket matching to support language constructs like
    `if/else/end`, `do/while`, HTML tags, etc.
    - Based on AstroCommunity's `motion/vim-matchup` pack
    - Disables matchparen highlighting in insert mode and enables deferred
    matching
    
    Closes #98
    
    ## Test plan
    - [ ] Open a Lua file and press `<Leader>m` on `if` to jump to matching
    `end`
    - [ ] Verify `%` works on standard brackets `()`, `[]`, `{}`
    - [ ] Verify matchparen highlighting does not appear in insert mode

---
+++ programs/nvim/config/lua/plugins/vim-matchup.lua
@@ -0,0 +1,24 @@
+-- https://github.com/andymass/vim-matchup
+-- Based on https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/motion/vim-matchup/init.lua
+return {
+  "andymass/vim-matchup",
+  event = "User AstroFile",
+  specs = {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      optional = true,
+      opts = { matchup = { enable = true } },
+    },
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        options = {
+          g = {
+            matchup_matchparen_nomode = "i",
+            matchup_matchparen_deferred = 1,
+          },
+        },
+      },
+    },
+  },
+}
