7712512fa430615997b9721731eb6ae5b1232e0b
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-08 21:54:05 +0900

    fix(nvim): increase indent line blend to 95

--- programs/nvim/config/lua/plugins/snacks.lua
+++ programs/nvim/config/lua/plugins/snacks.lua
@@ -1,9 +1,9 @@
 return {
   "folke/snacks.nvim",
   init = function()
-    vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#1e1e2e", blend = 90 })
+    vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#1e1e2e", blend = 95 })
     vim.api.nvim_create_autocmd("ColorScheme", {
-      callback = function() vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#1e1e2e", blend = 90 }) end,
+      callback = function() vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#1e1e2e", blend = 95 }) end,
     })
   end,
   opts = {
