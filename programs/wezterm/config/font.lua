99201657b69adb0f901ff4f5893240b8253e816b
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-10 22:13:38 +0900

    feat(wezterm): enable subpixel anti-aliasing for font rendering

--- programs/wezterm/config/font.lua
+++ programs/wezterm/config/font.lua
@@ -5,6 +5,8 @@ local M = {}
 function M.apply_to_config(config)
   config.font = wezterm.font("UbuntuSansMono Nerd Font Mono")
   config.font_size = 13.0
+  config.freetype_load_target = "Light"
+  config.freetype_render_target = "HorizontalLcd"
 end
 
 return M
