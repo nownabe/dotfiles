b883b28ef6cfc5918e5b356dda38251f881ce4da
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-10 22:09:55 +0900

    fix(wezterm): use correct monospace Nerd Font family name

--- programs/wezterm/config/font.lua
+++ programs/wezterm/config/font.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local M = {}
 
 function M.apply_to_config(config)
-  config.font = wezterm.font("UbuntuSans Nerd Font")
+  config.font = wezterm.font("UbuntuSansMono Nerd Font Mono")
   config.font_size = 13.0
 end
 
