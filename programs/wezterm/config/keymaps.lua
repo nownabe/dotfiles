a2d8b658bebf7967083ccbf442baf77cc076b1b8
nownabe <1286807+nownabe@users.noreply.github.com>
2026-02-11 14:50:43 +0900

    fix(wezterm): swap tab navigation direction

--- programs/wezterm/config/keymaps.lua
+++ programs/wezterm/config/keymaps.lua
@@ -24,8 +24,8 @@ function M.apply_to_config(config)
     { key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
 
     -- Tab navigation
-    { key = "h", mods = "LEADER", action = act.ActivateTabRelative(-1) },
-    { key = "l", mods = "LEADER", action = act.ActivateTabRelative(1) },
+    { key = "h", mods = "LEADER", action = act.ActivateTabRelative(1) },
+    { key = "l", mods = "LEADER", action = act.ActivateTabRelative(-1) },
     { key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
 
     -- Workspace (session) navigation
