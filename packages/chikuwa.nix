af168abd99c668edfce41c9812fb99de35ce5081
nownabe <1286807+nownabe@users.noreply.github.com>
2026-03-11 00:31:44 +0900

    chore(nix): update chikuwa to v0.1.7

--- packages/chikuwa.nix
+++ packages/chikuwa.nix
@@ -6,11 +6,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "chikuwa";
-  version = "0.1.6";
+  version = "0.1.7";
 
   src = fetchurl {
     url = "https://github.com/nownabe/chikuwa/releases/download/v${version}/chikuwa-x86_64-unknown-linux-gnu.tar.gz";
-    hash = "sha256-JhYtKjYt3LfaqRzzTxCCWCf+FEnjfGFbuXjRiPq39kM=";
+    hash = "sha256-gBuzI0yhxb2N2lwFI3If43tERoxlZOYWzuXssoTSmAE=";
   };
 
   sourceRoot = ".";
