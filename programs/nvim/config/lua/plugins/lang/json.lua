1877cc253ae0bd0edb8e362c2d5a640de9416c98
nownabe <1286807+nownabe@users.noreply.github.com>
2026-03-13 23:44:41 +0900

    feat(nvim): add JSON language support

---
+++ programs/nvim/config/lua/plugins/lang/json.lua
@@ -0,0 +1,50 @@
+-- JSON language support
+-- LSP: jsonls with schemastore
+
+return {
+  {
+    "b0o/schemastore.nvim",
+    lazy = true,
+  },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "jsonls" })
+      opts.config = vim.tbl_deep_extend("force", opts.config or {}, {
+        jsonls = {
+          on_new_config = function(config)
+            if not config.settings.json then config.settings.json = {} end
+            config.settings.json.schemas = require("schemastore").json.schemas()
+            config.settings.json.validate = { enable = true }
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "json", "jsonc" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "jsonls" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "json-lsp",
+      })
+    end,
+  },
+}
