dff73ee4eeb4e5da8a9283673da26a242adaf3cb
nownabe <1286807+nownabe@users.noreply.github.com>
2026-03-13 23:44:40 +0900

    feat(nvim): add TailwindCSS language support

---
+++ programs/nvim/config/lua/plugins/lang/tailwindcss.lua
@@ -0,0 +1,28 @@
+-- TailwindCSS language support
+-- LSP: tailwindcss
+
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "tailwindcss" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "tailwindcss" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "tailwindcss-language-server",
+      })
+    end,
+  },
+}
