3158318052ad9d8978b5651381e562fc5cfdea22
nownabe <1286807+nownabe@users.noreply.github.com>
2026-03-13 23:44:48 +0900

    feat(nvim): add CUE language support

---
+++ programs/nvim/config/lua/plugins/lang/cue.lua
@@ -0,0 +1,37 @@
+-- CUE language support
+-- LSP: dagger (cuelsp)
+
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "dagger" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "cue" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dagger" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "cuelsp",
+      })
+    end,
+  },
+}
