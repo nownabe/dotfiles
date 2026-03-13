9a345a6751718dd2be41ee681b406672a660c772
nownabe <1286807+nownabe@users.noreply.github.com>
2026-03-13 23:44:36 +0900

    feat(nvim): add Python language support

---
+++ programs/nvim/config/lua/plugins/lang/python.lua
@@ -0,0 +1,70 @@
+-- Python language support
+-- LSP: basedpyright, Formatter/Linter: ruff
+
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "basedpyright", "ruff" })
+      opts.config = vim.tbl_deep_extend("force", opts.config or {}, {
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              disableOrganizeImports = true, -- use ruff instead
+              analysis = {
+                autoImportCompletions = true,
+                diagnosticMode = "openFilesOnly",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "python" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "basedpyright", "ruff" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "basedpyright",
+        "ruff",
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_organize_imports", "ruff_format" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        python = { "ruff" },
+      },
+    },
+  },
+}
