df72dad6f6a6e73a3bd30ddc3c778728a47d37b6
Shogo Watanabe <1286807+nownabe@users.noreply.github.com>
2026-03-14 17:09:26 +0900

    feat(nvim): add TOML language support (#253)
    ## Summary
    - Add TOML language support with taplo LSP (includes built-in
    formatting)
    - Configure treesitter parser for toml
    
    Closes #131

---
+++ programs/nvim/config/lua/plugins/lang/toml.lua
@@ -0,0 +1,46 @@
+-- TOML language support
+-- LSP: taplo, Formatting: oxfmt
+
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "taplo" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "toml" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "taplo" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "taplo",
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        toml = { "oxfmt" },
+      },
+    },
+  },
+}
