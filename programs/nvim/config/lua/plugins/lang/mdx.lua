0d4b572e8f678bb424295b2741ac6f91f3d4f7e3
nownabe <1286807+nownabe@users.noreply.github.com>
2026-03-14 16:27:19 +0900

    feat(nvim): add oxfmt formatter for MDX

--- programs/nvim/config/lua/plugins/lang/mdx.lua
+++ programs/nvim/config/lua/plugins/lang/mdx.lua
@@ -1,19 +1,19 @@
 -- MDX language support
--- LSP: mdx_analyzer
+-- LSP: mdx_analyzer, Formatter: oxfmt
 
 return {
   {
     "AstroNvim/astrolsp",
     optional = true,
     opts = function(_, opts)
-      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "mdx_analyzer" })
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "mdx_analyzer", "oxfmt" })
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "mdx_analyzer" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "mdx_analyzer", "oxfmt" })
     end,
   },
   {
@@ -22,6 +22,7 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "mdx-analyzer",
+        "oxfmt",
       })
     end,
   },
