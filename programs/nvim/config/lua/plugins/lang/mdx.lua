72a7eb246a5774f1ba1f385d00575a93f2b62933
Shogo Watanabe <1286807+nownabe@users.noreply.github.com>
2026-03-14 17:12:50 +0900

    feat(nvim): add MDX language support (#255)
    ## Summary
    - Add MDX language support with mdx_analyzer LSP
    
    Closes #133

---
+++ programs/nvim/config/lua/plugins/lang/mdx.lua
@@ -0,0 +1,37 @@
+-- MDX language support
+-- LSP: mdx_analyzer, Formatting: oxfmt
+
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "mdx_analyzer" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "mdx_analyzer" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "mdx-analyzer",
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        mdx = { "oxfmt" },
+      },
+    },
+  },
+}
