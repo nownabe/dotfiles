bd8051b6e3f333663d867e282a5d322ed4a2ad1a
Shogo Watanabe <1286807+nownabe@users.noreply.github.com>
2026-03-14 18:47:16 +0900

    feat(nvim): add Astro language support (#260)
    ## Summary
    - Add Astro language support with astro LSP
    - Configure treesitter parser for astro
    
    Closes #139

---
+++ programs/nvim/config/lua/plugins/lang/astro.lua
@@ -0,0 +1,37 @@
+-- Astro language support
+-- LSP: astro
+
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "astro" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "astro" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "astro" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "astro-language-server",
+      })
+    end,
+  },
+}
