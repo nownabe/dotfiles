9844f48443aebc38389ec12026727c1b53a2b9f7
Shogo Watanabe <1286807+nownabe@users.noreply.github.com>
2026-03-14 18:46:20 +0900

    feat(nvim): add CUE language support (#258)
    ## Summary
    - Add CUE language support with dagger (cuelsp) LSP
    - Configure treesitter parser for cue
    
    Closes #137

---
+++ programs/nvim/config/lua/plugins/lang/cue.lua
@@ -0,0 +1,46 @@
+-- CUE language support
+-- LSP: cue lsp (official), Formatting: cue fmt
+
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "cue" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "cue" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "cue" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "cue",
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        cue = { "cue_fmt" },
+      },
+    },
+  },
+}
