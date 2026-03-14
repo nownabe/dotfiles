da4c07d446d8e30465a6a3986bdf2aa5c55466d0
Shogo Watanabe <1286807+nownabe@users.noreply.github.com>
2026-03-14 17:11:34 +0900

    feat(nvim): add Markdown language support (#254)
    ## Summary
    - Add Markdown language support with marksman LSP
    - Configure treesitter parsers for markdown and markdown_inline
    
    Closes #132

---
+++ programs/nvim/config/lua/plugins/lang/markdown.lua
@@ -0,0 +1,47 @@
+-- Markdown language support
+-- LSP: marksman, Formatting: oxfmt
+
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "marksman" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed =
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "markdown", "markdown_inline" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "marksman" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "marksman",
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        markdown = { "oxfmt" },
+      },
+    },
+  },
+}
