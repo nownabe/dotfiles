8446ed6c64ef88e15b51afa78d611e1072f67ebd
Shogo Watanabe <1286807+nownabe@users.noreply.github.com>
2026-03-14 16:31:13 +0900

    feat(nvim): add YAML language support (#252)
    ## Summary
    - Add YAML language support with yamlls LSP and schemastore.nvim
    integration
    - Configure treesitter parser for yaml
    - Disable built-in schemaStore in favor of schemastore.nvim
    
    Closes #130

---
+++ programs/nvim/config/lua/plugins/lang/yaml.lua
@@ -0,0 +1,60 @@
+-- YAML language support
+-- LSP: yamlls with schemastore, Formatter: oxfmt
+
+return {
+  {
+    "b0o/schemastore.nvim",
+    lazy = true,
+  },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = require("astrocore").list_insert_unique(opts.servers or {}, { "yamlls", "oxfmt" })
+      opts.config = vim.tbl_deep_extend("force", opts.config or {}, {
+        yamlls = {
+          on_new_config = function(config)
+            if not config.settings.yaml then config.settings.yaml = {} end
+            config.settings.yaml.schemas =
+              vim.tbl_extend("force", config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
+          end,
+          settings = {
+            yaml = {
+              validate = true,
+              schemaStore = {
+                enable = false, -- use schemastore.nvim instead
+                url = "",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "yaml" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "yamlls", "oxfmt" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "yaml-language-server",
+        "oxfmt",
+      })
+    end,
+  },
+}
