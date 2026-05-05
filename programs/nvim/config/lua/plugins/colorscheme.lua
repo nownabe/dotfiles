e68e420b0e887120a4f35a51f3e54c5ec7aa4914
nownabe <1286807+nownabe@users.noreply.github.com>
2026-05-05 11:47:57 +0900

    fix(nvim): set catppuccin priority higher than astrocore to fix transparency

--- programs/nvim/config/lua/plugins/colorscheme.lua
+++ programs/nvim/config/lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
-    priority = 1000,
+    priority = 10001,
     opts = {
       flavour = "mocha",
       transparent_background = true,
